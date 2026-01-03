3596f8d4a0f093cba82776659c05b389691241de
mcjava20 <litten_iop@126.com>
2026-01-03 21:33:35 +0800

    Delete pycpplink directory

--- pycpplink/__init__.py
+++
@@ -1,4 +0,0 @@
-from .wrapper import MemoryPool, AdvancedProcessor, Pointer, PyCPPError
-
-__version__ = '0.1.0'
-__all__ = ['MemoryPool', 'AdvancedProcessor', 'Pointer', 'PyCPPError', '__version__']
